8b1767a6ada88cffc9f1c179162855f7e0d89b0f
Ingrid Wong <yw340@georgetown.edu>
2018-01-13 22:43:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,16 @@
 
 def prime?(num)
-  if num == 1 || num ==-1
-    return false
-  end
- if num > 0
-  possible_factors = (2..num-1).to_a
-else
-  small=num+1
-  #puts small
-  #possible_factors = ((-(num+1))..-1).to_a
-  possible_factors= (small..-2).to_a
-end
-  possible_factors.each do |factor|
+
+ if num < 1
+
+   return false
+ else
+   (2..num-1).to_a.each do |factor|
     if num % factor == 0
       return false
     end
+    return true
   end
-
-  return true
 end
 
 prime?(-9)
