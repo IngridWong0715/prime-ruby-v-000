7331e76111f1fe3233b5ec8829300f5c4b53b82f
Ingrid Wong <yw340@georgetown.edu>
2018-01-13 22:46:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,3 @@
-def prime?(num)
- if num < 1
-   return false
- else
-   (2..num-1).to_a.each do |factor|
-    if num % factor == 0
-      return false
-    end
-  end
-  return true
-end
 
 def prime?(num)
   if num < 1
